ac16d179c63622d44ca6e35489af7319987b2eef
oliver-qveen <roman-savchenko2000@yandex.ru>
2020-05-01 18:01:12 +0300

    Добавлен SDK

--- Klondike Solitaire/Audio.h
+++ Klondike Solitaire/Audio.h
@@ -1,1 +1,5 @@
 #pragma once
+
+
+
+#include "Game.h"
--- Klondike Solitaire/Game.h
+++ Klondike Solitaire/Game.h
@@ -1,5 +1,11 @@
 #pragma once
+
 #include <Windows.h>
+
+#include "Audio.h"
+#include "Input.h"
+#include "Video.h"
+#include "Shell.h"
 
 class GameEngine
 {
