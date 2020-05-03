12957059d1eae8f16eef9e99bc39e924e25e1195
ASKET-asket <59205646+ASKET-asket@users.noreply.github.com>
2020-05-03 20:05:15 +0300

    Создана правильная иерархия фалов

---
+++ Development/Klondike Solitaire/Audio.cpp
@@ -0,0 +1,25 @@
+#include "Audio.h"
+
+AudioCore::AudioCore(HWND wnd)
+{}
+
+AudioCore::~AudioCore()
+{}
+
+void AudioCore::PlayBackground(int SoundIndex)
+{}
+
+void AudioCore::PlayForward(int SoundIndex)
+{}
+
+void AudioCore::SetBackgroundVolume(int Volume)
+{}
+
+void AudioCore::SetForwardVolume(int Volume)
+{}
+
+void AudioCore::StopBackground()
+{}
+
+void AudioCore::GetBackgroundIndex()
+{}
---
+++ Development/Klondike Solitaire/Game.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "Audio.h"
+#include "Input.h"
+#include "Video.h"
+#include "Shell.h"
+
+class GameEngine
+{
+
+};
