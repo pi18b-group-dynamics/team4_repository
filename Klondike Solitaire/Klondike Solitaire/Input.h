837f815247b308cc6e2bf2fbf5f9be0232b378dd
ASKET-asket <59205646+ASKET-asket@users.noreply.github.com>
2020-06-16 14:38:42 +0300

    Savchenko Nikolay
    Функции ввода

--- Klondike Solitaire/Klondike Solitaire/Input.h
+++ Klondike Solitaire/Klondike Solitaire/Input.h
@@ -1,15 +1,35 @@
 #pragma once
 
-#include "Game.h"
+#include <dinput.h>
 
-class InputCore
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+class Input
 {
 public:
-	InputCore();
-	~InputCore();
-	bool Init();
+	Input();
+	~Input();
+	bool Init(HINSTANCE hinstance, HWND hwnd, int screenWidth, int screenHeight);
 	void Destroy();
-	void Run();
-private:
+	bool Frame();
 
+	bool IsEscapePressed();
+	bool IsMousePressed();
+	void GetMouseLocation(int& mouseX, int& mouseY);
+
+private:
+	bool ReadKeyboard();
+	bool ReadMouse();
+	void ProcessInput();
+
+	IDirectInput8* m_directInput;
+	IDirectInputDevice8* m_keyboard;
+	IDirectInputDevice8* m_mouse;
+
+	unsigned char m_keyboardState[256];
+	DIMOUSESTATE m_mouseState;
+
+	int m_screenWidth, m_screenHeight;
+	int m_mouseX, m_mouseY;
 };
